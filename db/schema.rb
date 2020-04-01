0c6f4e3111ef54ca113d94020f3987935a2884c4
Joel <realjoelrivera@gmail.com>
2020-04-01 14:54:23 -0400

    Added comments onto reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_174000) do
+ActiveRecord::Schema.define(version: 2020_04_01_182929) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "review_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["review_id"], name: "index_comments_on_review_id"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
